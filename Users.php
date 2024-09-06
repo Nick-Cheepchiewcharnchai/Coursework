2fa278cef8d8ec36457edbc2e60daac1b35ada48
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2024-09-06 15:21:49 +0100

    .

--- Users.php
+++ Users.php
@@ -1,8 +1,25 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
   <title>Crosby Merch</title>
 </head>
+
+<body>
+
+<form action="addusers.php">
+  First name:<input type="text" name="forename"><br>
+  Last name:<input type="text" name="surname"><br>
+  Username:<input type="text" name="username"><br>
+  Password:<input type="password" name="passwd"><br>
+  <br>
+
+  <input type="radio" name="role" value="Pupil" checked> Pupil<br>
+  <input type="radio" name="role" value="Teacher"> Teacher<br>
+  <input type="radio" name="role" value="Admin"> Admin<br>
+  <input type="submit" value="Add User">
+</form>
 
 </body>
 </html>
