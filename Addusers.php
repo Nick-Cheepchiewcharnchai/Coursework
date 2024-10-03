54d6badea7d0f8ee75f39f51a888ca554d95c96a
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2024-10-03 11:15:12 +0100

    works now :)

--- Addusers.php
+++ Addusers.php
@@ -1,27 +1,27 @@
 <?php
 
-header('Location:users.php');
+header('Location:tryusers.php');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 try{
-
-	include_once("connection.php")
+	include_once("connection.php");
+	array_map("htmlspecialchars", $_POST);
 
 	switch($_POST["authority"]){
 		case "User":
 			$authority=0;
 			break;
 		case "Admin":
-			$authority=1;
+			$authority=2;
 			break;
 	}
 
 	$stmt = $conn->prepare("INSERT INTO tblusers (UserID,Firstname,Lastname,Username,Password,Authority)VALUES (null,:firstname,:lastname,:username,:password,:authority)");
-
-	#HASH PASSWORDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+	
+    #$hashed_password = password_hash($_POST["Pword"], PASSWORD_DEFAULT);
 
 	$stmt->bindParam(':firstname', $_POST["firstname"]);
 	$stmt->bindParam(':lastname', $_POST["lastname"]);
