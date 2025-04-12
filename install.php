456a97d74dc67023f4b657c65c7f3130da59bf4e
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-12 20:03:12 +0100

    TblUsers

--- install.php
+++ install.php
@@ -13,4 +13,16 @@ Authority TINYINT(1))");
 $stmt->execute();
 $stmt->closeCursor();
 
+//inserting user and admin data into the table
+$stmt = $conn->prepare("INSERT INTO Tblusers(Firstname, Lastname, Username, Password, Authority)
+VALUES('Nick', 'Cheepchiewcharnchai', 'Cheepchiewcharnch.k', :upassword, 0),
+      ('Admin', 'Install', 'admin', :apassword, 1)");
+
+$hashed_upassword = password_hash("1234", PASSWORD_DEFAULT);
+$stmt->bindParam(':upassword', $hashed_upassword);
+
+$hashed_apassword = password_hash("admin", PASSWORD_DEFAULT);
+$stmt->bindParam(':apassword', $hashed_apassword);
+$stmt->execute();
+
 ?>
