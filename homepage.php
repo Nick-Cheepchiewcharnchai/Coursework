821948dec674c5bbe5be48a7fe54ff42ddca0822
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-13 09:48:34 +0100

    .

--- homepage.php
+++ homepage.php
@@ -11,7 +11,15 @@
 
     <nav class="navbar navbar-expand-lg" style="background-color: grey;">
         <div class="container-fluid">
-            Navbar
+            <div class="navbar-header">
+                <a class="navbar-brand" href="homepage.php">Crosby Merch</a>
+            </div>
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="homepage.php">Browse</a></li>
+                <li class="nav-item"><a class="nav-link" href="basket.php">Basket</a></li>
+                <li class="nav-item"><a class="nav-link" href="purchases.php">Purchases</a></li>
+                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+            </ul>
         </div>
     </nav>
 
