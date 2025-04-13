4003775681ab2f0506251ab2e03381406181e1d8
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-13 10:50:36 +0100

    navbar

--- homepage.php
+++ homepage.php
@@ -14,14 +14,22 @@
             <a class="navbar-brand" href="homepage.php">
                 <img src="Crosby-Logo.jpg" alt="Crosby"> Crosby Merch
             </a>
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="homepage.php">Browse</a></li>
-                <li class="nav-item"><a class="nav-link" href="basket.php">Basket</a></li>
-                <li class="nav-item"><a class="nav-link" href="purchases.php">Purchases</a></li>
-                <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
-            </ul>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="homepage.php">Browse</a></li>
+                    <li class="nav-item"><a class="nav-link" href="basket.php">Basket</a></li>
+                    <li class="nav-item"><a class="nav-link" href="purchases.php">Purchases</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 </html>
