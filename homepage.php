b0164451f8ac1f08919b12005ae0c00e6dffeadc
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-13 13:22:26 +0100

    .

--- homepage.php
+++ homepage.php
@@ -13,22 +13,24 @@
     <?php include("navbar.php"); ?>
 
     <div class="container mt-5">
-        <?php
-            include_once("connection.php");
+        <div class="row" id="itemContainer">
+            <?php
+                include_once("connection.php");
 
-            $stmt = $conn->prepare("SELECT * FROM tblitems");
-            $stmt->execute();
+                $stmt = $conn->prepare("SELECT * FROM tblitems");
+                $stmt->execute();
 
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                echo('<div class="col-lg-3 item-card" data-type="' . $row['Itemtype'] . '">');
-                echo('<a style="text-decoration:none; color:inherit;" href="itemdisplay.php?IID=' . $row["ItemID"] . '">');
-                echo('<div><img src="/Coursework/Coursework-1/Pictures/' . $row["Picfront"] . '" width="200" height="200"></div>');
-                echo('<div class="item-name"><b>' . $row["Itemname"] . '</b></div>');
-                echo('<div class="item-price">£' . $row["Itemcost"] . '</div>');
-                echo('</a>');
-                echo('</div>');
-            }
-        ?>
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo('<div class="col-lg-3 item-card" data-type="' . $row['Itemtype'] . '">');
+                    echo('<a style="text-decoration:none; color:inherit;" href="itemdisplay.php?IID=' . $row["ItemID"] . '">');
+                    echo('<div><img src="/Coursework/Coursework-1/Pictures/' . $row["Picfront"] . '" width="200" height="200"></div>');
+                    echo('<div class="item-name"><b>' . $row["Itemname"] . '</b></div>');
+                    echo('<div class="item-price">£' . $row["Itemcost"] . '</div>');
+                    echo('</a>');
+                    echo('</div>');
+                }
+            ?>
+        </div>
     </div>
             
 </body>
