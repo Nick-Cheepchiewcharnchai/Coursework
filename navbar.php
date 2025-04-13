f7c515a01bdefe3fda17c4b0f67817dfaf5bfe02
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-13 11:14:59 +0100

    navbar

--- navbar.php
+++ navbar.php
@@ -1,20 +1,26 @@
+<!-- Navbar Section -->
 <nav class="navbar navbar-expand-lg custom-navbar">
     <div class="container-fluid">
+        <!-- Navbar brand with a logo and text -->
         <a class="navbar-brand" href="homepage.php">
             <img src="Crosby-Logo.jpg" alt="Crosby"> Crosby Merch
         </a>
+        <!-- Navbar toggle button for smaller screens (for mobile view) -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <!-- Navbar menu items -->
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item"><a class="nav-link" href="homepage.php">Browse</a></li>
-                <li class="nav-item"><a class="nav-link" href="basket.php">Basket</a></li>
+                 <!-- Navigation links for different pages -->
+                 <li class="nav-item"><a class="nav-link" href="homepage.php">Browse</a></li>
+                 <li class="nav-item"><a class="nav-link" href="basket.php">Basket</a></li>
                 <li class="nav-item"><a class="nav-link" href="purchases.php">Purchases</a></li>
                 <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
             </ul>
         </div>
     </div>
-</nav>
+ </nav>
 
+ <!-- Bootstrap JS bundle (needed for navbar toggle) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
