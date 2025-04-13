c4772d0be552b663410b13d1f5fa556c656f9570
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-13 10:02:53 +0100

    logout

--- logout.php
+++ logout.php
@@ -1,13 +1,8 @@
 <?php
-// Starts the session to access session variables
-session_start();
 
-// Checks if the session variable 'name' is set (i.e., the user is logged in)
-if(isset($_SESSION['name'])){
-    // Unsets the session variable 'name' to log the user out
-    unset($_SESSION['name']);
+if(isset($_SESSION['Name'])){
+    unset($_SESSION['Name']);
 }
 
-// Redirects the user to the login page after logging out
 header("Location: login.php");
 ?>
