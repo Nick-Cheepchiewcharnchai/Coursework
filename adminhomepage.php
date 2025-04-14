e75ed0d2ddc603f8ee5d51332ed0513b67baa362
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-14 09:48:25 +0100

    adminhomepage

--- adminhomepage.php
+++ adminhomepage.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crosby Merch</title>
-    <!-- Link to the Bootstrap CSS for styling and making the page responsive -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Link to a custom CSS file for additional styling -->
     <link href="mystyle.css" rel="stylesheet">
 </head>
 <body>
@@ -14,9 +12,7 @@
     <?php include("adminloggedin.php"); ?>
     <?php include("adminnavbar.php"); ?>
 
-    <!-- Main content area -->
     <div class="container mt-5">
-        <!-- Links to navigate to different sections of the admin panel -->
         <h1><a style="text-decoration:none; color:inherit;" href="items.php">Items</a></h1><br>
         <h1><a style="text-decoration:none; color:inherit;" href="orders.php">Orders</a></h1><br>
         <h1><a style="text-decoration:none; color:inherit;" href="accounts.php">Accounts</a></h1><br>
