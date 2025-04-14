210c0f417e2e3a2984c8ce1128168ace93058a31
Nick-Cheepchiewcharnchai <cheepchiewcharnch.k@oundleschool.org.uk>
2025-04-14 12:29:06 +0100

    .

--- additems.php
+++ additems.php
@@ -13,38 +13,41 @@
   <?php include("adminloggedin.php"); ?>
   <?php include("adminnavbar.php"); ?>
 
+  <!-- Main Content Section -->
   <div class="container mt-5">
     <h1>Add item</h1><br>
-
+    <!-- Form to add an item, submits to 'addingitem.php' -->
     <form action="addingitem.php" method="post" enctype="multipart/form-data">
       <div class="row">
         <div class="col">
+          <!-- Section to upload front and back pictures of the item -->
           <div class="row">
             <div class="col">
-              Front picture:</br><input class="form-control" type="file" id="picfront" name="picfront" accept="image/*" required><br>
+              Front picture:</br><input class="form-control" type="file" id="picfront" name="picfront" accept="image/*" required><br> <!-- File input for front picture -->
             </div>
             <div class="col">
-              Back picture:</br><input class="form-control" type="file" id="picback" name="picback" accept="image/*" required><br>
+              Back picture:</br><input class="form-control" type="file" id="picback" name="picback" accept="image/*" required><br> <!-- File input for back picture -->
             </div>
           </div>
         </div>
         <div class="col">
-          Item name:</br><input type="text" class="form-control" name="itemname" required><br>
-          Item price:</br><input type="text" class="form-control" name="itemcost" required><br>
-          Item description:</br><textarea class="form-control" name="itemdescription" rows="4" cols="50"></textarea><br>
+          <!-- Section for entering item details (name, price, description, type) -->
+          Item name:</br><input type="text" class="form-control" name="itemname" required><br> <!-- Text input for item name -->
+          Item price:</br><input type="text" class="form-control" name="itemcost" required><br> <!-- Text input for item price -->
+          Item description:</br><textarea class="form-control" name="itemdescription" rows="4" cols="50"></textarea><br> <!-- Textarea for item description -->
           
+          <!-- Dropdown to select item type -->
           Item type:</br><select class="form-select" name="itemtype" required>
-            <option value="T">Tops</option>
-            <option value="B">Bottoms</option>
-            <option value="A">Accessories</option>
-            <option value="O">Others</option>
+            <option value="T">Tops</option> <!-- Option for Tops -->
+            <option value="B">Bottoms</option> <!-- Option for Bottoms -->
+            <option value="A">Accessories</option> <!-- Option for Accessories -->
+            <option value="O">Others</option> <!-- Option for Other categories -->
           </select></br>
 
-          </br><input type="submit" class="confirm-button" value="Add Item">
+          </br><input type="submit" class="confirm-button" value="Add Item"> <!-- Submit button -->
         </div>
       </div>
     </form>
-    
   </div>
 
 </body>
